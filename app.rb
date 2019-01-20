8dacc0af335e2d26865b8e6631dbdb1efd0bde8e
thesecretmaster <thesecretmaster@developingtechnician.com>
2019-01-20 10:32:43 -0600

    Changed as requested by Petter in Trogdor on Jan 12

--- app.rb
+++ app.rb
@@ -9,10 +9,11 @@ settings = YAML.load_file('settings.yml')
 @cb = ChatBot.new(settings['chatx_username'], settings['chatx_password'])
 cli = SE::API::Client.new(settings['api_key'], site: 'stackoverflow')
 @cb.login(cookie_file: 'cookie.yml')
-
+ROOMS = settings['rooms'].keys
 def say(msg)
-  puts "I SPEAK: #{msg}"
-  @cb.say msg, 167908, server: 'stackoverflow'
+  ROOMS.each do |room|
+    @cb.say msg, room, server: 'stackoverflow'
+  end
 end
 
 say "Starting up!"
@@ -33,15 +34,18 @@ existant_tags = tags.each_slice(50).map do |batch|
   cli.tags(*batch)
 end.flatten.reject(&:nil?).reject { |t| t.count == 0 }
 
-if existant_tags.empty?
-  say "Check completed! No tags have risen from the dead!"
-else
-  uri = "https://stackoverflow.com/questions/tagged/"
-  say "Uh oh, I still see some tags! Specifically these tags:"
-  puts existant_tags.map(&:name).join(';')
-  existant_tags.sort_by(&:count).reverse.map { |t| "[tag:#{t.name}] _(×#{t.count})_" }.each_slice(4) do |to_speak|
-    puts "LAST TAG: #{to_speak.last}"
-    say to_speak.join(' ')
-    sleep 2
+settings['rooms'].each do |room_id, max_q_in_tag|
+  tags = existant_tags.reject { |t| t.count > max_q_in_tag }
+  if tags.empty?
+    @cb.say "Check completed! No tags have risen from the dead (unless the tags have > #{max_q_in_tag} questions)", room_id, server: 'stackoverflow'
+  else
+    uri = "https://stackoverflow.com/questions/tagged/"
+    @cb.say "Uh oh, I still see some tags! Specifically these tags:", room_id, server: 'stackoverflow'
+    puts tags.map(&:name).join(';')
+    tags.sort_by(&:count).reverse.map { |t| "[tag:#{t.name}] _(×#{t.count})_" }.each_slice(4) do |to_speak|
+      puts "LAST TAG: #{to_speak.last}"
+      @cb.say to_speak.join(' '), room_id, server: 'stackoverflow'
+      sleep 2
+    end
   end
 end
